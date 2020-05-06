6e322e79239533cb9483b3f18aef8399465e2610
Jim Ntare <jim.ntare@gmail.com>
2020-05-06 15:34:16 +0200

    ft(about section):
    implement the layout of the about section
    implement the css of the aobut section
    
    [ Finishes #172320169 ]

--- src/components/mainContent.js
+++ src/components/mainContent.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import Intro from './sections/Intro'
+import Intro from './sections/Intro';
+import About from './sections/About';
 
 const mainContent = () => {
   return (
     <div className='main-content'>
       <Intro />
+      <About />
     </div>
   );
 }
