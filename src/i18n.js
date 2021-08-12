fa6e7119314da900744e5f5bae074d4b26bf1b0c
Jim Ntare <jntare@Jims-BK-MacBook-Pro.local>
2021-08-12 09:21:23 +0200

    make final touches

--- src/i18n.js
+++ src/i18n.js
@@ -32,8 +32,8 @@ i18n
           about: {
             heading: 'About me',
             para1: 'Hi, I\'m Jim – Software Engineer / Blogger from Kigali. I\'m interested in everything technology and entreprenuership.',
-            para2: 'Formerly at Andela, I’m currently a Software Engineer at Bank Of Kigali and contributing to open source and pursuing personal projects.',
-            para3: 'Hire me to build you a website, moblie application, or speak at your event.',
+            para2: 'Formerly at Andela, I’m currently a Software Engineer at Bank Of Kigali; I\'m also contributing to open source and pursuing personal projects.',
+            para3: 'Hire me to build you a website, mobile application, or speak at your event.',
             para4: 'Ready to implement excellent projects with wonderful people.'
           },
           skills: {
